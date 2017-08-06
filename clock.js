41267a8dc99f1121a891f1bc267c197ad1059c4f
Matthew Morgan <matt@dulcetsoftware.com>
2017-08-06 08:47:41 -0400

    Complete solution

--- clock.js
+++ clock.js
@@ -1,5 +1,101 @@
-class Clock {
+const SMALL_TIME_NAMES = {
+  '00': 'twelve',
+  '01': 'one',
+  '02': 'two',
+  '03': 'three',
+  '04': 'four',
+  '05': 'five',
+  '06': 'six',
+  '07': 'seven',
+  '08': 'eight',
+  '09': 'nine',
+  '10': 'ten',
+  '11': 'eleven',
+  '12': 'twelve',
+  '13': 'thirteen',
+  '14': 'fourteen',
+  '15': 'fifteen',
+  '16': 'sixteen',
+  '17': 'seventeen',
+  '18': 'eighteen',
+  '19': 'nineteen'
+};
 
+const BIG_TIME_NAMES = {
+  '20': 'twenty',
+  '30': 'thirty',
+  '40': 'forty',
+  '50': 'fifty'
+};
+
+
+class Clock {
+  constructor(digits) {
+    this.digits = digits;
+    this.hours = this.digits.split(':')[0];
+    this.mins = this.digits.split(':')[1];
+    this.partOfDay = this.speakPartOfDay();
+  }
+
+  speak() {
+    return {
+          '00:00': 'midnight',
+          '12:00': 'midday'
+        }[this.digits]
+        || [this.speakHours(), this.speakMinutes(), `in the ${this.partOfDay}`].join(' ');
+  }
+
+  speakHours() {
+    let result;
+
+    let hourNumber = Number(this.hours);
+    if (hourNumber > 11) {
+      let adjHour = String(hourNumber - 12);
+      if (adjHour.length === 1) {
+        adjHour = `0${adjHour}`;
+      }
+      return SMALL_TIME_NAMES[adjHour];
+    }
+    return SMALL_TIME_NAMES[this.hours];
+  }
+
+  speakMinutes() {
+    let minuteNumber = Number(this.mins);
+
+    if (minuteNumber >= 20) {
+      let result;
+
+      let remainder = minuteNumber % 10;
+      let remainderText = '';
+      if (remainder) {
+        result = String(minuteNumber - remainder);
+        remainderText = ' ' + SMALL_TIME_NAMES[`0${remainder}`];
+      }
+      return `${BIG_TIME_NAMES[result || this.mins]}${remainderText}`;
+    }
+
+    if (minuteNumber >= 10) {
+      return SMALL_TIME_NAMES[this.mins];
+    }
+
+    if (minuteNumber === 0) {
+      return "o'clock";
+    }
+
+    return `oh ${SMALL_TIME_NAMES[this.mins]}`;
+  }
+
+  speakPartOfDay() {
+    let hourNumber = Number(this.hours);
+    if (hourNumber >= 18) {
+      return 'evening';
+    }
+    if (hourNumber >= 12) {
+      return 'afternoon';
+    }
+    return 'morning';
+  }
 }
+
 
 export default Clock;
