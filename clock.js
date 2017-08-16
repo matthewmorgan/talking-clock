5c003d5550c1355615422cca1787bd3be87a2b68
David Braley <david.braley@gmail.com>
2017-08-16 13:04:28 -0400

    refactor

--- clock.js
+++ clock.js
@@ -14,13 +14,9 @@ class Clock {
         }
 
         const said_hour = this.sayHour();
-        if (this.hour > 11 && this.hour < 18) {
-            return said_hour + " o'clock in the afternoon";
-        } else if (this.hour >= 18) {
-            return said_hour + " o'clock in the evening";
-        } else {
-            return said_hour + " o'clock in the morning";
-        }
+        const said_time_of_day = this.sayTimeOfDay();
+
+        return said_hour + " o'clock in the " + said_time_of_day;
     }
 
     sayHour() {
@@ -32,6 +28,16 @@ class Clock {
             return "three";
         } else if (this.hour === 18) {
             return "six";
+        }
+    }
+
+    sayTimeOfDay() {
+        if (this.hour < 12) {
+            return 'morning';
+        } else if (this.hour < 18) {
+            return 'afternoon';
+        } else {
+            return 'evening';
         }
     }
 }
