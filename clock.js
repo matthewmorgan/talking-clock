45df50532531b9e25a61d626eed57e6855e6cae9
David Braley <david.braley@gmail.com>
2017-08-16 15:33:46 -0400

    can say all times correctly

--- clock.js
+++ clock.js
@@ -14,12 +14,12 @@ class Clock {
             return 'midday';
         }
 
-        return this.sayHour() + " " +
-            this.sayMinutes() + " in the "
-            + this.sayTimeOfDay();
+        return this.speakHours() + " " +
+            this.speakMinutes() + " "
+            + this.speakPartOfDay();
     }
 
-    sayHour() {
+    speakHours() {
         let hourIn12HourFormat = this.hour % 12;
         if (hourIn12HourFormat === 0) {
             return 'twelve';
@@ -28,17 +28,17 @@ class Clock {
         }
     }
 
-    sayTimeOfDay() {
+    speakPartOfDay() {
         if (this.hour < 12) {
-            return 'morning';
+            return 'in the morning';
         } else if (this.hour < 18) {
-            return 'afternoon';
+            return 'in the afternoon';
         } else {
-            return 'evening';
+            return 'in the evening';
         }
     }
 
-    sayMinutes() {
+    speakMinutes() {
         if (this.minutes === 0) {
             return "o'clock";
         } else if (this.minutes < 10) {
@@ -49,17 +49,32 @@ class Clock {
     }
 
     static sayNumber(number) {
-        if (number === 1) {
-            return "one";
-        } else if (number === 2) {
-            return "two";
-        } else if (number === 3) {
-            return "three";
-        } else if (number === 6) {
-            return "six";
-        } else if (number === 10) {
-            return 'ten';
+        switch(number) {
+            case 1: return 'one';
+            case 2: return 'two';
+            case 3: return 'three';
+            case 4: return 'four';
+            case 5: return 'five';
+            case 6: return 'six';
+            case 7: return 'seven';
+            case 8: return 'eight';
+            case 9: return 'nine';
+            case 10: return 'ten';
+            case 11: return 'eleven';
+            case 12: return 'twelve';
+            case 13: return 'thirteen';
+            case 14: return 'fourteen';
+            case 15: return 'fifteen';
+            case 16: return 'sixteen';
+            case 17: return 'seventeen';
+            case 18: return 'eighteen';
+            case 19: return 'nineteen';
+            case 20: return 'twenty';
+            case 30: return 'thirty';
+            case 40: return 'forty';
+            case 50: return 'fifty';
         }
+        return Clock.sayNumber(Math.floor(number/10) * 10) + ' ' + Clock.sayNumber(number % 10);
     }
 }
 
--- clock.spec.js
+++ clock.spec.js
@@ -100,127 +100,127 @@ describe('the talking clock', () => {
         expect(clock.speak()).toEqual("three oh one in the morning")
     });
 
-    xtest('can speak the hour of the day', () => {
+    test('can speak the hour of the day', () => {
         const clock = new Clock('08:00');
 
         expect(clock.speakHours()).toEqual('eight');
     });
 
-    xtest('can speak the hour in the afternoon', () => {
+    test('can speak the hour in the afternoon', () => {
         const clock = new Clock('13:00');
 
         expect(clock.speakHours()).toEqual('one');
     });
 
-    xtest('can speak the top of the hour', () => {
+    test('can speak the top of the hour', () => {
         const clock = new Clock('08:00');
 
         expect(clock.speakMinutes()).toEqual("o'clock");
     });
 
-    xtest('can speak single digit minutes', () => {
+    test('can speak single digit minutes', () => {
         const clock = new Clock('08:01');
 
         expect(clock.speakMinutes()).toEqual("oh one");
     });
 
-    xtest('can speak double digit minutes', () => {
+    test('can speak double digit minutes', () => {
         const clock = new Clock('08:10');
 
         expect(clock.speakMinutes()).toEqual('ten');
     });
 
-    xtest('can speak twenty past', () => {
+    test('can speak twenty past', () => {
         const clock = new Clock('08:20');
 
         expect(clock.speakMinutes()).toEqual('twenty');
     });
 
-    xtest('can speak thirty-five past', () => {
+    test('can speak thirty-five past', () => {
         const clock = new Clock('08:35');
 
         expect(clock.speakMinutes()).toEqual('thirty five');
     });
 
-    xtest('knows when it\'s morning', () => {
+    test('knows when it\'s morning', () => {
         const clock = new Clock('08:00');
 
         expect(clock.speakPartOfDay()).toEqual('in the morning');
     });
 
-    xtest('knows when it\'s afternoon', () => {
+    test('knows when it\'s afternoon', () => {
         const clock = new Clock('13:00');
 
         expect(clock.speakPartOfDay()).toEqual('in the afternoon');
     });
 
-    xtest('knows when it\'s evening', () => {
+    test('knows when it\'s evening', () => {
         const clock = new Clock('18:00');
 
         expect(clock.speakPartOfDay()).toEqual('in the evening');
     });
 
-    xtest('knows how to describe 8 AM', () => {
+    test('knows how to describe 8 AM', () => {
         const clock = new Clock('08:00');
 
         expect(clock.speak()).toEqual("eight o'clock in the morning");
     });
 
-    xtest('knows how to describe 8:10 AM', () => {
+    test('knows how to describe 8:10 AM', () => {
         const clock = new Clock('08:10');
 
         expect(clock.speak()).toEqual("eight ten in the morning");
     });
 
-    xtest('knows how to describe 8:15 AM', () => {
+    test('knows how to describe 8:15 AM', () => {
         const clock = new Clock('08:15');
 
         expect(clock.speak()).toEqual("eight fifteen in the morning");
     });
 
-    xtest('knows how to describe 8:20 AM', () => {
+    test('knows how to describe 8:20 AM', () => {
         const clock = new Clock('08:20');
 
         expect(clock.speak()).toEqual("eight twenty in the morning");
     });
 
-    xtest('knows how to describe 8:30 AM', () => {
+    test('knows how to describe 8:30 AM', () => {
         const clock = new Clock('08:30');
 
         expect(clock.speak()).toEqual("eight thirty in the morning");
     });
 
-    xtest('knows how to describe 8:55 AM', () => {
+    test('knows how to describe 8:55 AM', () => {
         const clock = new Clock('08:55');
 
         expect(clock.speak()).toEqual("eight fifty five in the morning");
     });
 
-    xtest('knows how to describe 12:15 PM', () => {
+    test('knows how to describe 12:15 PM', () => {
         const clock = new Clock('12:15');
 
         expect(clock.speak()).toEqual("twelve fifteen in the afternoon");
     });
 
-    xtest('knows how to describe 1:00 PM', () => {
+    test('knows how to describe 1:00 PM', () => {
         const clock = new Clock('13:00');
 
         expect(clock.speak()).toEqual("one o'clock in the afternoon");
     });
 
-    xtest('knows how to describe 1:30 PM', () => {
+    test('knows how to describe 1:30 PM', () => {
         const clock = new Clock('13:30');
 
         expect(clock.speak()).toEqual("one thirty in the afternoon");
     });
 
-    xtest('knows how to describe 12:01 AM', () => {
+    test('knows how to describe 12:01 AM', () => {
         const clock = new Clock('00:01');
 
         expect(clock.speak()).toEqual("twelve oh one in the morning");
     });
 
-    xtest('knows how to describe 6 PM', () => {
+    test('knows how to describe 6 PM', () => {
         const clock = new Clock('18:00');
 
         expect(clock.speak()).toEqual("six o'clock in the evening");
